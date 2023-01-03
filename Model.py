f814a18861a6e367cbea1008cce6a9231ac4e118
WyllasSidjeno <perreaultchristopher1984@gmail.com>
2023-01-02 19:47:50 -0500

    Model :: Added Format text to path method

--- Model.py
+++ Model.py
@@ -8,3 +8,9 @@ class MainModel:
 
     def set_data(self, data):
         self.data = data
+
+
+def format_text_to_path(name, imgtype=".png", destination="./"):
+    name = name.lower().replace(" ", "_")
+    path = destination + name + imgtype
+    return path
