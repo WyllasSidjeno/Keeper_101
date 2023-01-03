ed6cf373c3139c2f88264a1a37c8d9055d2e7779
WyllasSidjeno <perreaultchristopher1984@gmail.com>
2023-01-02 19:46:59 -0500

    Model :: Added main model
    Get data, set data

---
+++ Model.py
@@ -0,0 +1,10 @@
+
+class MainModel:
+    def __init__(self):
+        self.data = None
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, data):
+        self.data = data
