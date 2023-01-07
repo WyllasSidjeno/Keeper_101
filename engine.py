b3da8b99752e5c67b2be193ba25afd0eb49e293d
WyllasSidjeno <perreaultchristopher1984@gmail.com>
2023-01-07 18:50:14 -0500

    Engine :: Added docstring

--- engine.py
+++ engine.py
@@ -1,9 +1,9 @@
+"""This represents the engine of the application."""
 import sys
 from PyQt6.QtWidgets import QApplication
 from Presenter import MainPresenter
 
 if __name__ == '__main__':
-    # Create the main presentation and a app using pyqt6
     app = QApplication(sys.argv)
     presentation = MainPresenter()
     presentation.run()
